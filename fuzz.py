4640ffba77a71a1dc8a99afaf672e00b1bf7b37f
Fern <fdiaz@gitlab.com>
2022-03-04 17:46:56 +0000

    Update fuzz.py

--- fuzz.py
+++ fuzz.py
@@ -1,11 +1,17 @@
 from pythonfuzz.main import PythonFuzz
+from html.parser import HTMLParser
 
 
 @PythonFuzz
 def fuzz(buf):
-    pass
+    try:
+        string = buf.decode("ascii")
+        parser = HTMLParser()
+        parser.feed(string)
+    except UnicodeDecodeError:
+        pass
+
 
 
 if __name__ == '__main__':
     fuzz()
-
