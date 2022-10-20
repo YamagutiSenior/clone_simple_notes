3ee65a274ea1f1d62a0dfce4de27e91b7862b98e
Fernando Diaz <fdiaz@gitlab.com>
2022-10-20 10:41:36 -0600

    Revamp scripts

--- scripts/delete_latest.sh
+++ scripts/delete_latest.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 docker image prune --force
 if [ $retVal -ne 0 ]; then
     echo "Error: Could not prune images"
     echo "Maybe they doesn't exist"
 fi
 
-docker image delete $IMAGE
+docker image rm $IMAGE
 if [ $retVal -ne 0 ]; then
     echo "Error: Could not delete image."
     echo "Maybe it doesn't exist"
--- scripts/helm_reset.sh
+++ scripts/helm_reset.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 DEPLOY=notes
 
 helm delete $DEPLOY
