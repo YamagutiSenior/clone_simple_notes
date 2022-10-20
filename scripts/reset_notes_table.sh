828fef890e4e2b7c462f72f7f7dda0103dacd63f
Fernando Diaz <fdiaz@gitlab.com>
2022-10-20 11:23:11 -0600

    Updates

--- scripts/reset_notes_table.sh
+++ scripts/reset_notes_table.sh
@@ -6,22 +6,22 @@ DB_NAME='my_database'
 USER=root
 EXISTS_CMD="SELECT * FROM information_schema.tables WHERE table_schema = '$DB_NAME' AND table_name = 'notes' LIMIT 1;"
 DROP_CMD="DROP TABLE notes;"
-CREATE_CMD="CREATE TABLE IF NOT EXISTS notes (id integer NOT NULL AUTO_INCREMENT,data text,PRIMARY KEY (id);'"
+CREATE_CMD="CREATE TABLE IF NOT EXISTS notes (id integer NOT NULL AUTO_INCREMENT, data text, PRIMARY KEY (id));"
 
 # Check if table exists
-EXISTS=$($DB_CLIENT -u $USER --password=$DB_ROOT_PWD -h $DB_SERVER -e $EXISTS_CMD)
+EXISTS=$($DB_CLIENT -u $USER --password=$DB_ROOT_PWD -h $DB_SERVER -e "$EXISTS_CMD")
 if [ -z "$EXISTS" ]; then
     echo "Error: Table does not exists."
     echo "Attempting to create it."
     # Create it
-    $DB_CLIENT -u $USER --password=$DB_ROOT_PWD -h $DB_SERVER -e $CREATE_CMD $DB_NAME
+    $DB_CLIENT -u $USER --password=$DB_ROOT_PWD -h $DB_SERVER -e "$CREATE_CMD" $DB_NAME
     if [ $retVal -ne 0 ]; then
         echo "Error: Creating the table failed"
         exit $retVal
     fi
 else
     # If it does exist, delete it
-    $DB_CLIENT -u $USER --password=$DB_ROOT_PWD -h $DB_SERVER -e $DROP_CMD $DB_NAME
+    $DB_CLIENT -u $USER --password=$DB_ROOT_PWD -h $DB_SERVER -e "$DROP_CMD" $DB_NAME
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo "Error: Dropping the table failed."
@@ -29,7 +29,7 @@ else
     fi
 
     # Recreate it with empty values
-    $DB_CLIENT -u $USER --password=$DB_ROOT_PWD -h $DB_SERVER -e $CREATE_CMD $DB_NAME
+    $DB_CLIENT -u $USER --password=$DB_ROOT_PWD -h $DB_SERVER -e "$CREATE_CMD" $DB_NAME
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo "Error: Creating the table failed"
