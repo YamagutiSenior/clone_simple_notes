eb9a03cdf4916dfb37a7a9ab1569d8f1efea025e
Fernando Diaz <fdiaz@gitlab.com>
2022-10-23 14:10:17 -0600

    Script Updates

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 
 class Config(object):
     defaultKey = os.urandom(12).hex()
-    SECRET_KEY = os.environ.get('SECRET_KEY') or defaultKey
+    SECRET_KEY = os.environ.get('NOTES_SECRET_KEY') or defaultKey
--- scripts/delete_latest.sh
+++ scripts/delete_latest.sh
@@ -6,10 +6,13 @@ if [ $retVal -ne 0 ]; then
     echo "Maybe they doesn't exist"
 fi
 
-docker image rm $IMAGE
-if [ $retVal -ne 0 ]; then
-    echo "Error: Could not delete image."
-    echo "Maybe it doesn't exist"
+# Only delete the image if it's the latest
+if [$CI_COMMIT_REF_NAME == $CI_DEFAULT_BRANCH]; then
+    docker image rm $IMAGE
+    if [ $retVal -ne 0 ]; then
+        echo "Error: Could not delete image."
+        echo "Maybe it doesn't exist"
+    fi
 fi
 
 exit 0
