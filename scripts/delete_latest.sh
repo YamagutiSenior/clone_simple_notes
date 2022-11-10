a5159ff80d1aab2f2b71b63e707c507234d0aca5
Fern <fdiaz@gitlab.com>
2022-11-10 22:31:16 +0000

    Update scripts/delete_latest.sh, scripts/notes_reset.sh

--- scripts/delete_latest.sh
+++ scripts/delete_latest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker image prune --force
-if [ $retVal -ne 0 ]; then
+if [ $? -ne 0 ]; then
     echo "Error: Could not prune images"
     echo "Maybe they doesn't exist"
 fi
@@ -9,7 +9,7 @@ fi
 # Only delete the image if it's the latest
 if [$CI_COMMIT_REF_NAME == $CI_DEFAULT_BRANCH]; then
     docker image rm $IMAGE
-    if [ $retVal -ne 0 ]; then
+    if [ $? -ne 0 ]; then
         echo "Error: Could not delete image."
         echo "Maybe it doesn't exist"
     fi
--- scripts/notes_reset.sh
+++ scripts/notes_reset.sh
@@ -3,7 +3,7 @@
 NOTES=notes
 
 helm delete $NOTES
-if [ $retVal -ne 0 ]; then
+if [ $? -ne 0 ]; then
     echo "Error: Could not delete notes deployment."
     echo "Maybe it doesn't exist"
 fi
