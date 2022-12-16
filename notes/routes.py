955b184b22d5b739b5c1b1b1a42e09662c0c469c
Fernando Diaz <fdiaz@gitlab.com>
2022-12-15 18:07:35 -0600

    correction to delete logic

--- notes/routes.py
+++ notes/routes.py
@@ -250,11 +250,12 @@ def delete_note(id=None, admin=False):
     except Exception as e:
         return jsonify({"Error": str(e)}), 500
 
+    # TEST
     note.logger.info("ITEMS: {}".format(items))
     note.logger.info("ITEMS-LEN: {}".format(len(items)))
     note.logger.info("ITEMS-DICT: {}".format(items.__dict__))
 
-    if len(items) < 1:
+    if len(items) > 1:
         return jsonify({"Error": "Note still exists"}), 500
 
     return jsonify({"Success": "Note Deleted!"}), 204
