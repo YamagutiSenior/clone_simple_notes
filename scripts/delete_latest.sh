c8d8e92aee68ab1c26ea86f5ede00ff3aa1e4003
Fernando Diaz <awkwardferny@gmail.com>
2023-01-03 22:58:00 -0500

    Update Build and CleanDB images

--- scripts/delete_latest.sh
+++ scripts/delete_latest.sh
@@ -6,13 +6,10 @@ if [ $? -ne 0 ]; then
     echo "Maybe they doesn't exist"
 fi
 
-# Only delete the image if it's the latest
-if [$CI_COMMIT_REF_NAME == 'main'] || [$CI_COMMIT_REF_NAME == 'staging']; then
-    docker image rm $IMAGE
-    if [ $? -ne 0 ]; then
-        echo "Error: Could not delete image."
-        echo "Maybe it doesn't exist"
-    fi
+docker image rm $IMAGE
+if [ $? -ne 0 ]; then
+    echo "Error: Could not delete image."
+    echo "Maybe it doesn't exist"
 fi
 
 exit 0
