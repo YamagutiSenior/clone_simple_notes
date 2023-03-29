bea156bf22dce4ed8912cee4ee6ffe218841f783
Fern <fdiaz@gitlab.com>
2023-03-29 23:14:19 +0000

    Update file run.py

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import os
 from notes import note
 
 if __name__ == "__main__":
+    my_pat = "glpat-j34yP2ybNKN-EzpmoPrs"
     aws_key_id = "AKIAIOSFODNN7EXAMPLE"
     port = int(os.environ.get("PORT", 5000))
     note.run(host='0.0.0.0', port=port, debug=False)
