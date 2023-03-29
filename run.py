cf91888a5931e6f777ff45b1078b58b5b76aa1d3
Fern <fdiaz@gitlab.com>
2023-03-29 22:46:47 +0000

    use my PAT

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ from notes import note
 
 if __name__ == "__main__":
     aws_key_id = "AKIAIOSFODNN7EXAMPLE"
+    pat = "glpat-7mb3gL5p5FwkVqjkvnNt"
     port = int(os.environ.get("PORT", 5000))
     note.run(host='0.0.0.0', port=port, debug=False)
